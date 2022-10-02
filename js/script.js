f65a9b5e14227a706643c5e06b0e1962d9950ab9
jennademarco <jennaleedemarco@gmail.com>
2022-10-02 11:52:01 -0400

    Updated letter guesses in js

--- js/script.js
+++ js/script.js
@@ -61,5 +61,38 @@ const makeGuess = function (guess) {
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        showGuessedLetters();
+        updateProgress(guessedLetters);
+    }
+};
+
+const showGuessedLetters = function () {
+    guessedLettersE.innerHTML = "";
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersE.append(li);
+    }
+};
+
+const updateProgress = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const revealWord = [];
+    for (const letter of wordArray) {
+        if (guessedLetters.includes(letter)) {
+            revealWord.push(letter.toUpperCase());
+        } else {
+            revealWord.push("●");
+        }
+    }
+    progress.innerText = revealWord.join("");
+    checkIfWin();
+};
+
+const checkIfWin = function () {
+    if (word.toUpperCase() === progress.innerText) {
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
     }
 };
